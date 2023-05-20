c5bd885bf0fdbd44bc00fc77bf3d7f2e14180249
Cory Powell <coryrpowell@gmail.com>
2023-05-20 17:30:49 -0400

    Add answers

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,32 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+# The include question mark method is called on the string object "Hello World"
+# The method is asking if the string has the argument "Hello" which is in the parenthesis.
+# The string does include "Hello" so the return value would be True.
 "Hello World".include?("Hello")
 
 
+# The end with question mark method is calling on the string object "Hello World"
+# The method is asking if the string has the argument "Hello" at the end of the string.
+# "Hello World" does not have "Hello" at the end so it would return value as false.
 "Hello World".end_with?("Hello")
 
 
+# The end with question mark method is calling on the string object "Hello World"
+# The method is asking if the string has the argument "rld" at the end of the string. 
+# "Hello World" does end wiht "rld" at the end so it would return value as true. 
 "Hello World".end_with?("rld")
 
 
+# The even question mark method is asking if the integer 12 is an even number
+# 12 is an even number so the return value would be true.
 12.even?
 
 
+# The next method is called on the integer 18
+# The method is asking what will come next numerically after 18
+# The return value would be 19
 18.next
 
 
@@ -38,7 +51,11 @@
 # The puts command prints the return value of the start_with? method (true) to the console.
 first_name = "Jeff"
 puts first_name.start_with?("J")
-
+# The length method is called on the last_name variable. It stores the stringe object "Bridges".
+# The method returns the amount of characters in the string as a number.
+# The puts command prints the return value of the length method, which for this string "Bridges" is 7 characters long.
+last_name = "Bridges"
+puts last_name.length
 
 
 # SECTION 3: Calling methods on variables assigned to integers.
@@ -47,6 +64,18 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+# The odd? method is called on the current_age variable, which stores the integer object 32.
+# The method is a boolean that will return either true or false. It's asking if the integer is an odd number.
+# Because 32 is an even number, the puts command would print out false.
+current_age = 32
+puts 32.odd?
+
+
+# The pred method is called on the future_age variable, which stores the integer object 34.
+# The method returns the number preceeding the integer number.
+# So in this example, the puts command prints the number preceeding 34, which is 33.
+future_age = 34
+puts 34.pred
 
 
 # SECTION 4: Calling methods on variables assigned to arrays.
@@ -54,3 +83,15 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# The upcase method is being called on the string "Meg". It is an array element from the variable white_stripes
+# The method returns the chracters of the string in uppercase letters.
+# So the puts command is printing out "MEG" in uppercase letters.
+white_stripes = ["Meg", "Jack"]
+puts "Meg".upcase
+
+
+# The at method is being called on the index position 1. It is an element in the array white_stripes that has two total elements
+# Because Ruby starts with 0 as the first index position, the second position, in this case "Jack", is index position 1.
+# The puts command is printing out the variable white_stripes 1 which in this case is "Jack" 
+puts white_stripes.at(1)
